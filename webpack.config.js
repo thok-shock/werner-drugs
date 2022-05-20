3fe2c25280d488d26384bd335f9389d2378c9a9e
thok-shock <rswerner@wisc.edu>
2022-05-19 20:14:32 -0500

    production additions

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
@@ -6,31 +7,63 @@ const htmlPlugin = new HtmlWebPackPlugin({
 const path = require('path');
 const webpack = require("webpack");
 
-module.exports = {
-    mode: "development",
-    entry: {
-        index: ['./src/index.js', 'webpack-hot-middleware/client']
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
+
+if (process.env.NODE_ENV === 'development') {
+    module.exports = {
+        mode: "development",
+        entry: {
+            index: ['./src/index.js', 'webpack-hot-middleware/client']
         },
-        {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+            ]
+        },
+        devtool: 'inline-source-map',
+        plugins: [new webpack.HotModuleReplacementPlugin()],
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+            publicPath: '/'
         }
-        ]
-    },
-    devtool: 'inline-source-map',
-    plugins: [new webpack.HotModuleReplacementPlugin()],
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/'
-    }
-};
+    };
+} else if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        mode: "production",
+        entry: {
+            index: ['./src/index.js']
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+            ]
+        },
+        plugins: [htmlPlugin],
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+            publicPath: '/'
+        }
+    };
+}
+
